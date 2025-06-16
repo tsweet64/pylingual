3e1255936c92e626666fd6d5f4df883f175c50d6
Tim Sweet <tsweet64@protonmail.com>
2025-06-16 12:55:02 -0500

    Fixing some try edge cases

--- pylingual/control_flow_reconstruction/templates/Exception.py
+++ pylingual/control_flow_reconstruction/templates/Exception.py
@@ -121,7 +121,6 @@ class TryElse3_12(ControlFlowTemplate):
 @register_template(0, 1, (3, 10))
 class Try3_10(ControlFlowTemplate):
     template = T(
-        try_header=~N("try_body"),
         try_body=N("try_footer.", None, "except_body"),
         try_footer=~N("tail."),
         except_body=~N("tail.").of_subtemplate(Except3_10),
@@ -133,7 +132,6 @@ class Try3_10(ControlFlowTemplate):
             {
                 EdgeKind.Fall: "tail",
             },
-            "try_header",
             "try_body",
             "except_body",
             "try_footer",
@@ -143,17 +141,14 @@ class Try3_10(ControlFlowTemplate):
     @to_indented_source
     def to_indented_source():
         """
-        {try_header}
         try:
             {try_body}
         {except_body}
         """
 
-
 @register_template(0, 0, (3, 10))
 class TryElse3_10(ControlFlowTemplate):
     template = T(
-        try_header=N("try_body"),
         try_body=N("try_footer.", None, "except_body"),
         try_footer=N("else_body").with_in_deg(1),
         except_body=N("tail.").with_in_deg(1).of_subtemplate(Except3_10),
@@ -166,7 +161,6 @@ class TryElse3_10(ControlFlowTemplate):
             {
                 EdgeKind.Fall: "tail",
             },
-            "try_header",
             "try_body",
             "try_footer",
             "except_body",
@@ -177,7 +171,6 @@ class TryElse3_10(ControlFlowTemplate):
     @to_indented_source
     def to_indented_source():
         """
-        {try_header}
         try:
             {try_body}
         {except_body}
@@ -211,7 +204,7 @@ class NamedExc3_10(ExcBody3_10):
 
 class BareExcept3_10(Except3_10):
     template = T(
-        except_body=~N("tail.", None).of_type(BlockTemplate).with_cond(with_instructions("POP_EXCEPT")),
+        except_body=~N("tail.", None).of_type(BlockTemplate).with_cond(with_instructions("POP_EXCEPT"), with_instructions("RAISE_VARARGS")),
         tail=N.tail(),
     )
 
